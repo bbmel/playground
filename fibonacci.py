6ac731e857c3df48eaed5f3e1ce5237473c3353c
Mel <mmgravier@gmail.com>
2017-09-19 12:34:39 -0400

    fibonacci efficient algorithm

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+#python3
+
+def fib(n):
+    fibList = []
+    fibList.append(0)
+    fibList.append(1)
+    for i in range(2, n + 1):
+        n = fibList[i - 1] + fibList[i - 2]
+        fibList.append(n)
+    return (fibList[len(fibList) - 1])
+
+
+
