31ead389b0dbf82962a9d40cf7b891ff02184b2f
Mel <mmgravier@gmail.com>
2017-09-19 13:38:43 -0400

    fixed some more bugz

--- fibonacci.py
+++ fibonacci.py
@@ -7,7 +7,7 @@ def fib(n):
     for i in range(2, n + 1):
         n = fibList[i - 1] + fibList[i - 2]
         fibList.append(n)
-    print (fibList[len(fibList) - 1])
+    return(fibList[n - 1])
 
-
-
+n = int(input())
+print(fib(n))
--- fibonacci_last_digit.py
+++ fibonacci_last_digit.py
@@ -7,6 +7,9 @@ def fib(n):
     for i in range(2, n + 1):
         n = (fibList[i - 1] + fibList[i - 2]) % 10
         fibList.append(n)
-    print (fibList[len(fibList) - 1])
+    return(fibList[len(fibList) - 1])
+
+n = int(input())
+print(fib(n))
 
 
