2ecae6d6ec8e3d3e6c21852107b8e21588e3f554
Mel <mmgravier@gmail.com>
2017-09-19 13:50:43 -0400

    working submissions?

--- fibonacci.py
+++ fibonacci.py
@@ -7,7 +7,8 @@ def fib(n):
     for i in range(2, n + 1):
         n = fibList[i - 1] + fibList[i - 2]
         fibList.append(n)
-    return(fibList[n - 1])
+    return (fibList[len(fibList) - 1])
+
 
 n = int(input())
 print(fib(n))
