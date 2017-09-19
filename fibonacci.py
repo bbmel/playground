8c69167146c592aa9a793d85e80f58050434dabc
Mel <mmgravier@gmail.com>
2017-09-19 16:29:42 -0400

    final solutions
    no bugs, passed all tests

--- fibonacci.py
+++ fibonacci.py
@@ -2,13 +2,15 @@
 
 def fib(n):
     fibList = []
-    fibList.append(0)
-    fibList.append(1)
-    for i in range(2, n + 1):
-        n = fibList[i - 1] + fibList[i - 2]
-        fibList.append(n)
-    return (fibList[len(fibList) - 1])
-
+    if n is 0 or n is 1:
+        return(n)
+    else:
+        fibList.append(0)
+        fibList.append(1)
+        for i in range(2, n + 1):
+            valAtIndex = fibList[i - 1] + fibList[i - 2]
+            fibList.append(valAtIndex)
+        return(fibList[n])
 
 n = int(input())
 print(fib(n))
